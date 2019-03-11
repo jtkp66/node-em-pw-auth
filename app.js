b8bbe85c1770a7a6c45ad4bb6affd1ce86797351
Jamie Pham <jtkp66@gmail.com>
2019-03-11 14:48:54 -0700

    Able to register, login and redirect to dashboard page.

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
-// db config
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
 const db = require('./config/keys').MongoURI;
 
-// connnect to Mongo
-mongoose.connect(db, {useNewUrlParser: true })
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//EJS
+// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-//Bodyparser to get data from a form.
-app.use(express.urlencoded({ extended: false }));
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
 
